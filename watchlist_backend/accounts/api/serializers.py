6251afa80dbdb7a1c00682d9c23ccdb46da8585c
Erik Turøy Midtun <etm.midtun@gmail.com>
2020-02-14 18:24:24 +0100

    lagt til fornavn og etternavn i registreringsmodellen til users

--- watchlist_backend/accounts/api/serializers.py
+++ watchlist_backend/accounts/api/serializers.py
@@ -9,20 +9,23 @@ User._meta.get_field('email')._unique = True
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'username','email', 'first_name','last_name',)
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'username','email', 'password','first_name','last_name',)
         extra_kwargs = {'password': {'write_only': True}}
+    
 
     def create(self, validated_data):
         user = User.objects.create_user(
             validated_data['username'],
-            validated_data['email'],
-            validated_data['password']
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
         )
         return user
 
