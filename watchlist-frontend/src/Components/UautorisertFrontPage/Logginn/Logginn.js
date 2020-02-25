0fb9a7e286f05ed6021b93c7537a81303867538e
Erik Turøy Midtun <etm.midtun@gmail.com>
2020-02-25 21:33:38 +0100

    Refactoring + kommentarer

--- watchlist-frontend/src/Components/UautorisertFrontPage/Logginn/Logginn.js
+++ watchlist-frontend/src/Components/UautorisertFrontPage/Logginn/Logginn.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 
+/* Styling */
 import { Card, Button, Form } from "react-bootstrap/";
 
+/* Routing */
 import { Link, Redirect } from "react-router-dom";
 
+/* Redux */
 import { connect } from "react-redux";
 import { login } from "../../../actions/auth";
 
@@ -11,34 +14,35 @@ class Logginn extends Component {
   constructor() {
     super();
     this.state = {
-      username: '',
-      password: ''
-    }
+      username: "",
+      password: ""
+    };
+    /* Gjør at vi kan kalle metodene i JSX i render */
     this.eventHandler = this.eventHandler.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
+
+  /* Tar ut verdien av det som blir skrevet i Form.Contol inputboksene og oppdaterer state*/
   eventHandler(event) {
     const target = event.target;
     const value = target.value;
     const name = target.name;
-
-    /* console.log(name,value); */
     this.setState({
       [name]: value
     });
   }
-  onSubmit(event) {
-    
-    const formValues = {
-      username: this.state.username,
-      password: this.state.password,
-    };
-    /* console.log("formvalue" + formValues); */
-    this.props.login(formValues);
 
-    /* console.log(this.props.isAuthenticated); */
-    event.preventDefault();
+  /* Hva som skal gjøres når logginn knappen blir trykket på */
+  onSubmit(event) {
+    const formValues = {
+      // hva som skal bli sendt til backenden
+      username: this.state.username,
+      password: this.state.password
+    };
+    this.props.login(formValues);
+    event.preventDefault(); // stopper nettleseren fra å prøve å redirecte oss
   }
+
   render() {
     if (this.props.isAuthenticated) {
       return <Redirect to="/" />;
@@ -51,11 +55,21 @@ class Logginn extends Component {
         <Form noValidate onSubmit={this.onSubmit}>
           <Form.Group controlId="formName">
             <Form.Label>Fornavn</Form.Label>
-            <Form.Control type="username" name="username" placeholder="navn@domene.com" onChange={this.eventHandler} />
+            <Form.Control
+              type="username"
+              name="username"
+              placeholder="navn@domene.com"
+              onChange={this.eventHandler}
+            />
           </Form.Group>
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Passord</Form.Label>
-            <Form.Control type="password" name="password" placeholder="Passord" onChange={this.eventHandler} />
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Passord"
+              onChange={this.eventHandler}
+            />
           </Form.Group>
           <Button variant="primary" type="submit" block>
             Log inn
@@ -70,6 +84,7 @@ class Logginn extends Component {
   }
 }
 
+/* Hvikle props vi vil ha fra redux store */
 const mapStateToProps = state => ({
   isAuthenticated: state.auth.isAuthenticated
 });
