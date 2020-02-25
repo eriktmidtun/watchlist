5c5e2d4877e1ef41369faaa762f1282997f00b71
David Bendiksen <davidbendiksen@hotmail.com>
2020-02-25 18:52:02 +0100

    forsøk på ferdiggjøring

--- watchlist-frontend/src/Components/UautorisertFrontPage/Logginn/Logginn.js
+++ watchlist-frontend/src/Components/UautorisertFrontPage/Logginn/Logginn.js
@@ -5,9 +5,33 @@ import { Card, Button, Form } from "react-bootstrap/";
 import { Link, Redirect } from "react-router-dom";
 
 import { connect } from "react-redux";
-import { login } from "../../actions/auth";
+import { login } from "../../../actions/auth";
 
 class Logginn extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: ''
+    }
+    this.eventHandler = this.eventHandler.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  eventHandler(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    //console.log(name,value);
+    this.setState({
+      [name]: value
+    });
+  }
+  onSubmit() {
+    const formValues = {
+      username: this.state.username,
+      password: this.state.password
+    }
+    this.props.login(formValues)
+  }
   render() {
     if (this.props.isAuthenticated) {
       return <Redirect to="/" />;
@@ -17,14 +41,14 @@ class Logginn extends Component {
         <Card.Title style={{ textAlign: "center", fontSize: "2em" }}>
           Log inn
         </Card.Title>
-        <Form noValidate onSubmit={onSubmit}>
+        <Form noValidate onSubmit={this.onSubmit}>
           <Form.Group controlId="formName">
             <Form.Label>Fornavn</Form.Label>
-            <Form.Control type="email" placeholder="navn@domene.com" />
+            <Form.Control type="username" placeholder="navn@domene.com" onChange={this.eventHandler} />
           </Form.Group>
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Passord</Form.Label>
-            <Form.Control type="password" placeholder="Passord" />
+            <Form.Control type="password" placeholder="Passord" onChange={this.eventHandler} />
           </Form.Group>
           <Button variant="primary" type="submit" block>
             Log inn
