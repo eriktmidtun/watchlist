f544681e03dfec3497dc41db0e4e159d4ddbcca2
Erik Turøy Midtun <etm.midtun@gmail.com>
2020-02-25 17:52:30 +0100

    la til registrerings action

--- watchlist-frontend/src/actions/auth.js
+++ watchlist-frontend/src/actions/auth.js
@@ -1,20 +1,23 @@
-import axios from "axios";
-/* import { stopSubmit } from 'redux-form';  */
+import axios from 'axios';
+/* import { stopSubmit } from 'redux-form'; */
 
 import {
   USER_LOADING,
   USER_LOADED,
   AUTH_ERROR,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
-} from "./types";
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS
+} from './types';
 
 // LOAD USER
 export const loadUser = () => async (dispatch, getState) => {
   dispatch({ type: USER_LOADING });
 
   try {
-    const res = await axios.get("/api/auth/user", tokenConfig(getState));
+    const res = await axios.get('/api/auth/user', tokenConfig(getState));
     dispatch({
       type: USER_LOADED,
       payload: res.data
@@ -26,12 +29,38 @@ export const loadUser = () => async (dispatch, getState) => {
   }
 };
 
+// REGISTER USER
+export const register = ({ username, email, password }) => async dispatch => {
+  // Headers
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  // Request Body
+  const body = JSON.stringify({ username, email, password });
+
+  try {
+    const res = await axios.post('/api/auth/register', body, config);
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: REGISTER_FAIL
+    });
+    /* dispatch(stopSubmit('registerForm', err.response.data)); */
+  }
+};
+
 // LOGIN USER
 export const login = ({ username, password }) => async dispatch => {
   // Headers
   const config = {
     headers: {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json'
     }
   };
 
@@ -39,7 +68,7 @@ export const login = ({ username, password }) => async dispatch => {
   const body = JSON.stringify({ username, password });
 
   try {
-    const res = await axios.post("/api/auth/login", body, config);
+    const res = await axios.post('/api/auth/login', body, config);
     dispatch({
       type: LOGIN_SUCCESS,
       payload: res.data
@@ -48,8 +77,16 @@ export const login = ({ username, password }) => async dispatch => {
     dispatch({
       type: LOGIN_FAIL
     });
-    /*     dispatch(stopSubmit('loginForm', err.response.data)); */
+   /*  dispatch(stopSubmit('loginForm', err.response.data)); */
   }
+};
+
+// LOGOUT USER
+export const logout = () => async (dispatch, getState) => {
+  await axios.post('/api/auth/logout', null, tokenConfig(getState));
+  dispatch({
+    type: LOGOUT_SUCCESS
+  });
 };
 
 // helper function
@@ -60,12 +97,12 @@ export const tokenConfig = getState => {
   // Headers
   const config = {
     headers: {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json'
     }
   };
 
   if (token) {
-    config.headers["Authorization"] = `Token ${token}`;
+    config.headers['Authorization'] = `Token ${token}`;
   }
 
   return config;
