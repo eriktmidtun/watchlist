ece84f5921261ff957a4646a1f460bc9a6105e5a
larshfl <larshfl@stud.ntnu.no>
2020-03-10 13:02:59 +0100

    MovieItem komponent

--- watchlist-frontend/src/Components/Profil/Liste.js
+++ watchlist-frontend/src/Components/Profil/Liste.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { Card } from "react-bootstrap/";
+import MovieItem from "./MovieItem"
 
-const Liste = ({ listeNavn}) => {
+
+
+
+
+const Liste = ({listeNavn}) => {
   return (
     <Card style={{padding: "32px" }}>
       <Card.Title style={{ textAlign: "center", fontSize: "2em" }}>
         {listeNavn}
       </Card.Title>
       <ul> {/* Disse er bare midleridige */}
-      <li>Film 1</li>
+        <li>
+          <MovieItem id={550}>
+          </MovieItem>
+        </li>
       <li>Film 2</li>
       <li>Film 3</li>
       <li>Film 4</li>
