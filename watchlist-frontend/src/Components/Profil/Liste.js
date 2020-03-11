d37f29a4b40387a1131bb25243922de4f0351026
larshfl <larshfl@stud.ntnu.no>
2020-03-11 13:28:15 +0100

    La til MediaItem komponent

--- watchlist-frontend/src/Components/Profil/Liste.js
+++ watchlist-frontend/src/Components/Profil/Liste.js
@@ -1,26 +1,22 @@
 import React from "react";
 import { Card } from "react-bootstrap/";
-import MovieItem from "./MovieItem"
+import MediaItem from "./MediaItem"
 
 
 
-
+// se på map
 
 const Liste = ({listeNavn}) => {
+
+  let ids = [1, 2, 3, 4, 5,5,5,5,5,5]
+
+
   return (
     <Card style={{padding: "32px" }}>
-      <Card.Title style={{ textAlign: "center", fontSize: "2em" }}>
+      <Card.Title style={{ textAlign: "center", fontSize: "2em" }} >
         {listeNavn}
       </Card.Title>
-      <ul> {/* Disse er bare midleridige */}
-        <li>
-          <MovieItem id={550}>
-          </MovieItem>
-        </li>
-      <li>Film 2</li>
-      <li>Film 3</li>
-      <li>Film 4</li>
-      </ul>
+        {ids.map( (i) => <MediaItem id={i}> </MediaItem>)}
     </Card>
   );
 };
