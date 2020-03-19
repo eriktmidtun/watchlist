1236da1dd52ab77e2d0bf15223d8dc8f67d49bb6
Erik Turøy Midtun <etm.midtun@gmail.com>
2020-03-19 20:41:46 +0100

    fikset retreive api by id for listene på backend

--- watchlist_backend/mediaItem/api/views.py
+++ watchlist_backend/mediaItem/api/views.py
@@ -1,7 +1,8 @@
 
-from rest_framework import viewsets, permissions, exceptions
+from rest_framework import  viewsets, permissions, exceptions
 from rest_framework.exceptions import  ValidationError
-
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 from .serializers import HaveWatchedMediaItemSerializer, WantToWatchMediaItemSerializer
 from mediaItem.models import HaveWatchedMediaItem, WantToWatchMediaItem
@@ -13,12 +14,20 @@ class wantToWatchMediaItemViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):  
         return self.request.user.wantToWatchMediaItem.all()
+    
+    def retrieve(self, request, pk):
+        queryset = request.user.wantToWatchMediaItem.all()
+        mediaItem= queryset.filter(mdbID = pk)
+        obj = get_object_or_404(mediaItem)
+        return Response(WantToWatchMediaItemSerializer(obj).data)
 
     def perform_create(self, serializer):  
         queryset = self.request.user.wantToWatchMediaItem.all().filter(mdbID = self.request.data['mdbID'])
         if queryset.exists():
             raise ValidationError('mediumet finnes allerede i listen')
         serializer.save(owner=self.request.user)
+
+
 
 class haveWatchedMediaItemViewSet(viewsets.ModelViewSet):
     serializer_class = HaveWatchedMediaItemSerializer
@@ -32,3 +41,12 @@ class haveWatchedMediaItemViewSet(viewsets.ModelViewSet):
         if queryset.exists():
             raise ValidationError('mediumet finnes allerede i listen')
         serializer.save(owner=self.request.user)
+    
+    def retrieve(self, request, pk):
+        queryset = request.user.haveWatchedMediaItem.all()
+        mediaItem= queryset.filter(mdbID = pk)
+        obj = get_object_or_404(mediaItem)
+        return Response(HaveWatchedMediaItemSerializer(obj).data)
+
+
+    
