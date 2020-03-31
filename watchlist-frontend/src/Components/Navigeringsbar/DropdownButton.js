299a75635ce8099fecddf5dddc0cde51586c34f0
erikmidtun <etm.midtun@gmail.com>
2020-03-31 18:25:59 +0200

    oppdaterte routing på dropdownbutton

--- watchlist-frontend/src/Components/Navigeringsbar/DropdownButton.js
+++ watchlist-frontend/src/Components/Navigeringsbar/DropdownButton.js
@@ -3,27 +3,17 @@ import React, { Component } from "react";
 /* Styling */
 import { ButtonGroup, Dropdown, Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+/* Routing */
+import { withRouter } from 'react-router-dom';
 
-export default class DropdownButton_1 extends Component {
+class DropdownButton extends Component {
   render() {
-    /* const OnCLickNameButton = () => {
-      console.log("Name Button pressed");
-    };
-
-    const OnCLickDropdown1 = () => {
-      console.log("Dropdown action 1 pressed");
-    };
-
-    const OnCLickDropdown2 = () => {
-      console.log("Dropdown action 2 pressed");
-    }; */
-
     const OnCLickDropdown = () => {
       this.props.logout();
     };
     return (
       <Dropdown as={ButtonGroup} className="ml-auto">
-          <Button href="/profil" variant="primary" type="button" style={{ minWidth: "200px" }}>
+          <Button onClick={() => this.props.history.push("/profil")} variant="primary" type="button" style={{ minWidth: "200px" }}>
             {this.props.name ? this.props.name.first_name : "Placeholder"}
           </Button>
         <Dropdown.Toggle split variant="primary" id="dropdown-split-basic" />
@@ -43,3 +33,5 @@ export default class DropdownButton_1 extends Component {
     );
   }
 }
+
+export default withRouter(DropdownButton);
--- watchlist-frontend/src/actions/constants.js
+++ watchlist-frontend/src/actions/constants.js
@@ -1,3 +1,3 @@
-/* Bytt denne addressen til http://admin.watchlist.social for å kjøre mot backend serveren vår */
+/* Bytt denne addressen til https://admin.watchlist.social for å kjøre mot backend serveren vår */
 export const backendBaseURL = `http://localhost:8000`;
 export const APIkey = 'c5733a52f13cedc8b47b7a21e8edd914';
