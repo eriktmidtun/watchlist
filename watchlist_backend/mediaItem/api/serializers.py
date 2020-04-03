622cc836d3799ca09673916ec55cfcc3cfc55e94
David Bendiksen <davidbendiksen@hotmail.com>
2020-04-03 13:32:33 +0200

    Added spacing

--- watchlist_backend/mediaItem/api/serializers.py
+++ watchlist_backend/mediaItem/api/serializers.py
@@ -5,10 +5,12 @@ Serializer for the mediaItem lists.
 from rest_framework import serializers
 from mediaItem.models import HaveWatchedMediaItem, WantToWatchMediaItem
 
+
 class HaveWatchedMediaItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = HaveWatchedMediaItem
         fields = ('mdbID','mediumType',)
+
 
 class WantToWatchMediaItemSerializer(serializers.ModelSerializer):
     class Meta:
